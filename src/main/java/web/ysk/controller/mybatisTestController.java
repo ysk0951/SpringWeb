3ab5a8c018eb16329c5d981d8e5595a6fcf59653
yskimWEB <abcm556@naver.com>
2020-06-22 12:52:52 +0900

    test Mybatis 20200622 1252

---
+++ src/main/java/web/ysk/controller/mybatisTestController.java
@@ -0,0 +1,8 @@
+package web.ysk.controller;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class mybatisTestController {
+
+}
---
+++ src/main/java/web/ysk/vo/TestDTO.java
@@ -0,0 +1,35 @@
+package web.ysk.vo;
+
+import java.sql.Timestamp;
+
+public class TestDTO {
+	private String id;
+	private String pw;
+	private int age;
+	private Timestamp reg;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Timestamp getReg() {
+		return reg;
+	}
+	public void setReg(Timestamp reg) {
+		this.reg = reg;
+	}
+	
+}
